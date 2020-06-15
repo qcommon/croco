c3f428cefba299cad7b7931f468f469b35d8293f
2xsaiko <git@dblsaiko.net>
2020-06-15 22:21:54 +0200

    Fix Matrix3fMixin implementing Matrix4fExt

--- src/main/java/net/dblsaiko/qcommon/croco/mixin/Matrix3fMixin.java
+++ src/main/java/net/dblsaiko/qcommon/croco/mixin/Matrix3fMixin.java
@@ -5,10 +5,10 @@ import net.minecraft.client.util.math.Matrix3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-import net.dblsaiko.qcommon.croco.ext.Matrix4fExt;
+import net.dblsaiko.qcommon.croco.ext.Matrix3fExt;
 
 @Mixin(Matrix3f.class)
-public abstract class Matrix3fMixin implements Matrix4fExt {
+public abstract class Matrix3fMixin implements Matrix3fExt {
 
     @Shadow protected float a00;
     @Shadow protected float a01;
